da1a775ca7088be6ea0576044b5cc3122a8deff6
GonzaloHaag <gonzalohaag13@gmail.com>
2025-02-27 15:52:27 -0300

    solution three

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.get('/api/hello', function (req, res) {
 //{"ipaddress":"159.20.14.100","language":"en-US,en;q=0.5","software":"Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:50.0) Gecko/20100101 Firefox/50.0"}
 app.get('/api/whoami',(req,res) => {
 
-  return res.json({ipaddress:req.ip,language:req.headers["accept-language"],software:req.headers["user-agent"]})
+  return res.json({"ipaddress":req.ip.split(',')[0].trim(),"language":req.headers["accept-language"],"software":req.headers["user-agent"]})
 })
 
 // listen for requests :)
